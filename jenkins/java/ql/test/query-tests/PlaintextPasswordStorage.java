e2d3d06b79e494f5f3ca519e4065d86a4cb87aac
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2022-05-03 22:20:21 +0200

    Exclude prefixedCredentialsId from plaintext password check (#15)
    Also add tests for this query and revise the inline message a bit
    
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>

---
+++ jenkins/java/ql/test/query-tests/PlaintextPasswordStorage.java
@@ -0,0 +1,21 @@
+import hudson.util.Secret;
+
+public class PlaintextPasswordStorage {
+    private static String PASSWORD;
+
+    private String password;
+
+    private String credentialsId;
+
+    private String prefixedCredentialsId;
+
+    private String credentialId;
+
+    private String prefixedCredentialId;
+
+    private String credential;
+
+    private String credentials;
+
+    private Secret secretPassword;
+}
